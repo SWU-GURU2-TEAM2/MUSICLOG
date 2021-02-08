2ecbba6cc0461bf70b84ac2f27fdd54d07eb7f24
madilyn <lollin0@naver.com>
2021-02-08 16:09:11 +0900

    SETTING

---
+++ MusicDiary/MusicDiary/UserStructure.swift
@@ -0,0 +1,15 @@
+//
+//  UserStructure.swift
+//  MusicDiary
+//
+//  Created by 1v1 on 2021/01/31.
+//
+
+import UIKit
+
+public struct UserStructure {
+    var userName: String?
+    var userId: String?
+    var userImage: UIImage?
+    var userDiaryList: [Int]?
+}
