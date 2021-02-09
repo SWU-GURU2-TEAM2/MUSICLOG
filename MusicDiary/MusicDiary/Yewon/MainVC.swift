e801a23d9f3174f6f2edfb90381a9f0e7a701482
yeew3658 <yeew3658@gmail.com>
2021-02-09 13:52:06 +0900

    Read Diary Data

--- MusicDiary/MusicDiary/Yewon/MainVC.swift
+++ MusicDiary/MusicDiary/Yewon/MainVC.swift
@@ -34,107 +34,99 @@ class MainVC:UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //Users>diaryList
-        db.collection("Users").getDocuments { (snapshot, error) in
-            if let error = error {
-                print(error)
-                return
-            }
-            if let snapshot = snapshot {
-                for getDoc in snapshot.documents {
-                    //print("\(getDoc.documentID) => \(getDoc.data()["userDiaryList"])")
-                    self.getDiaryList = getDoc.data()["userDiaryList"] as! [String]
-                    print("Diary List : ", self.getDiaryList)
-                }
-            }
-            //Diary>data
-            self.db.collection("Diary").order(by: "date").getDocuments { (snapshot, error) in
-                if let error = error {
-                    print(error)
-                    return
-                }
-                if let snapshot = snapshot {
-                    for document in snapshot.documents {
-                        print("\(document.documentID) => \(document.data())")
-                        for currentDId in self.getDiaryList {
-                            let docRef = self.db.collection("Diary").document("\(currentDId)")
-                            var newDiaryData = DiaryStructure()
-                            docRef.getDocument { (document, error) in
-                                if let document = document, document.exists {
-                                    let dataDescription = document.data()
-                                    //newContent.musicCoverUrl = URL(string: (dataDescription!["musicCoverUrl"]! as? String)!)
-                                    
-                                    newDiaryData.diaryName = dataDescription!["diaryName"] as? String
-                                    newDiaryData.diaryMusicTitle = dataDescription!["diaryMusicTitle"] as? String
-                                    newDiaryData.diaryMusicArtist = dataDescription!["diaryMusicArtist"] as? String
-                                    newDiaryData.diaryImageUrl = URL(string: (dataDescription!["diaryImageUrl"]! as? String)!)
-                                    //newDiaryData.memberList = dataDescription!["memberList"] as? [String]
-                                    
-                                    print("new data: ", newDiaryData)
-                                    self.diaryData.append(newDiaryData)
-                                    
-                                    
-                                } else {
-                                    print("Document does not exist")
-                                }
-                            }
+        //Users에서 diaryList 필드 읽어오기
+        db.collection("Users").document("h1jETajvj6NiFtA9qSE1VRjQ7AP2").getDocument { (document, error) in
+            if let document = document, document.exists {
+                let dataDescription = document.data()
+                self.getDiaryList = (dataDescription!["userDiaryList"] as? [String])!
+                print("diary list : ", self.getDiaryList)
+                
+                
+                
+                //Diary에서
+                for currentDId in self.getDiaryList {
+                    let docRef = self.db.collection("Diary").document("\(currentDId)")
+                    var newDiaryData = DiaryStructure()
+                    docRef.getDocument { (document, error) in
+                        if let document = document, document.exists {
+                            let dataDescription = document.data()
+                            //newContent.musicCoverUrl = URL(string: (dataDescription!["musicCoverUrl"]! as? String)!)
+                            
+                            newDiaryData.diaryName = dataDescription!["diaryName"] as? String
+                            newDiaryData.diaryMusicTitle = dataDescription!["diaryMusicTitle"] as? String
+                            newDiaryData.diaryMusicArtist = dataDescription!["diaryMusicArtist"] as? String
+                            newDiaryData.diaryImageUrl = URL(string: (dataDescription!["diaryImageUrl"]! as? String)!)
+                            //newDiaryData.memberList = dataDescription!["memberList"] as? [String]
+                            
+                            print("new data: ", newDiaryData)
+                            self.diaryData.append(newDiaryData)
+                            
+                        } else {
+                            print("Document does not exist")
+                            
                         }
-                        self.mainCarousel.reloadData()
+                        
                     }
                 }
+                self.mainCarousel.reloadData()
             }
         }
+        
+        
+        
+        
+        
         
     }
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-            super.viewWillTransition(to: size, with: coordinator)
-            mainCarousel.deviceRotated()
-        }
+        super.viewWillTransition(to: size, with: coordinator)
+        mainCarousel.deviceRotated()
+    }
 }
-    
-    class MainCell: ScalingCarouselCell {
-        @IBOutlet weak var mainDiaryImaage: UIImageView!
-        @IBOutlet weak var mainDiaryName: UILabel!
-        @IBOutlet weak var mainStartDate: UILabel!
+
+class MainCell: ScalingCarouselCell {
+    @IBOutlet weak var mainDiaryImaage: UIImageView!
+    @IBOutlet weak var mainDiaryName: UILabel!
+    @IBOutlet weak var mainStartDate: UILabel!
+}
+
+typealias CarouselDatasource = MainVC
+extension CarouselDatasource: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return getDiaryList.count
     }
     
-    typealias CarouselDatasource = MainVC
-    extension CarouselDatasource: UICollectionViewDataSource {
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return getDiaryList.count
-        }
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let carouselCell = collectionView.dequeueReusableCell(withReuseIdentifier: "carouselCell", for: indexPath) as! MainCell
+        //        let diaryCount = diaryData[indexPath.row]
+        //        let data0Fdiary = diaryCount.data()
+        //        //diary name insert
+        //        carouselCell.mainDiaryName.text = data0Fdiary["diaryName"] as! String
+        //        //date insert 0000년 00일 00일 형식으로 지정 필요
+        //        let starDate = data0Fdiary["date"]
+        //        //image insert
+        //        //print("Image Url : \(data0Fdiary["diaryImageUrl"])")
+        //        //carouselCell.mainStartDate.text = startDate
+        carouselCell.setNeedsLayout()
+        carouselCell.layoutIfNeeded()
         
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            let carouselCell = collectionView.dequeueReusableCell(withReuseIdentifier: "carouselCell", for: indexPath) as! MainCell
-            //        let diaryCount = diaryData[indexPath.row]
-            //        let data0Fdiary = diaryCount.data()
-            //        //diary name insert
-            //        carouselCell.mainDiaryName.text = data0Fdiary["diaryName"] as! String
-            //        //date insert 0000년 00일 00일 형식으로 지정 필요
-            //        let starDate = data0Fdiary["date"]
-            //        //image insert
-            //        //print("Image Url : \(data0Fdiary["diaryImageUrl"])")
-            //        //carouselCell.mainStartDate.text = startDate
-            carouselCell.setNeedsLayout()
-            carouselCell.layoutIfNeeded()
-            
-            return carouselCell
-        }
+        return carouselCell
+    }
 }
-    
-    typealias CarouselDelegate = MainVC
-    extension MainVC: UICollectionViewDelegate {
-        func scrollViewDidScroll(_ scrollView: UIScrollView) {
-            
-        }
+
+typealias CarouselDelegate = MainVC
+extension MainVC: UICollectionViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
     }
-    
-    private typealias ScalingCarouselFlowDelegate = MainVC
-    extension ScalingCarouselFlowDelegate: UICollectionViewDelegateFlowLayout {
-        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-            return 0
-        }
-        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-            return 0
-        }
+}
+
+private typealias ScalingCarouselFlowDelegate = MainVC
+extension ScalingCarouselFlowDelegate: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+}
