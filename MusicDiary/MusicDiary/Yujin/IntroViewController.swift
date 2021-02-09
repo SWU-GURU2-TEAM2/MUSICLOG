a311fd4c894eb350121ff1589c5b2909def2e3ab
Yu-jinKang <jgkyj@naver.com>
2021-02-10 08:44:35 +0900

    profile image to link

--- MusicDiary/MusicDiary/Yujin/IntroViewController.swift
+++ MusicDiary/MusicDiary/Yujin/IntroViewController.swift
@@ -59,7 +59,7 @@ class IntroViewController: UIViewController, FUIAuthDelegate, UIGestureRecognize
                         let imageRef = storageRef.child("profileImages/Icon material-person@3x.png")
                         self.db.collection("Users").document("\(currentUser.uid)").setData(["userDiaryList" : [],
                              "userID" : "\(currentUser.uid)",
-                             "userImage" : "\(imageRef.fullPath)",
+                             "userImage" : "https://firebasestorage.googleapis.com/v0/b/musicdiary-a095d.appspot.com/o/profileImages%2FIcon%20material-person%403x.png?alt=media&token=82fd8978-badd-4b3c-b4f9-ba655408362f",
                              "userName" : "\(currentUser.displayName!)"])
                         print("UserDataSaved")
                     }
